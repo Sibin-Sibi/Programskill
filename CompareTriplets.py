d3f712262699a8741332be0079cdca9c60c85b7c
sibinsiby <60280080+sibinsiby@users.noreply.github.com>
2020-02-11 17:41:06 -0500

    Solution Added

--- CompareTriplets.py
+++ CompareTriplets.py
@@ -17,6 +17,16 @@ import sys
 
 # Complete the compareTriplets function below.
 def compareTriplets(a, b):
+ x = 0
+    y = 0
+    for i in range(len(a)):
+        if a[i]> b[i]:
+            x = x + 1
+        else:
+            y = y + 1
+        if a[i] == b[i]:
+            y = y - 1
+    return [x,y]
  
 
 
