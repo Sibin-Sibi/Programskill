7c12c48b7df7d822dd1bd2b2fab7e366105c7a8d
sibinsiby <60280080+sibinsiby@users.noreply.github.com>
2020-02-11 17:38:30 -0500

    Comparison of triplets

---
+++ CompareTriplets.py
@@ -0,0 +1,35 @@
+"""
+Complete the function compareTriplets in the editor below. It must return an array of two integers, the first being Alice's score and the second being Bob's.
+
+compareTriplets has the following parameter(s):
+
+a: an array of integers representing Alice's challenge rating
+b: an array of integers representing Bob's challenge rating
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+ 
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
