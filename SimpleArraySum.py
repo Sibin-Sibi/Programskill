fa8d86e4a641004f2b22e2c1321f5fdbae9fee13
sibinsiby <sibinjoseph@gmail.com>
2020-02-11 02:10:48 -0500

    Solution Created

---
+++ SimpleArraySum.py
@@ -0,0 +1,47 @@
+# Given an array of integers, find the sum of its elements.
+
+#For example, if the array , , so return .
+
+#Function Description
+
+#Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+#simpleArraySum has the following parameter(s):
+
+#ar: an array of integers
+#Input Format
+
+#The first line contains an integer, , denoting the size of the array.
+#The second line contains  space-separated integers representing the array's elements.
+
+#Constraints
+
+
+#Output Format
+
+#print the sum of the array's elements as a single integer.
+
+#Solution
+
+import os
+import sys
+
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar):
+    
+        return sum(ar) # returns sum of the splitted list
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
